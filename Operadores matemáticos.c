de601311d1678fe978604466a54ba24f42133934
Hector2711 <castrillorodriguezhector@gmail.com>
2024-09-01 13:16:28 -0600

    Add files via upload

---
+++ Operadores matemáticos.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<locale.h>
+void  main(){
+	setlocale(LC_ALL,"spanish");
+		
+	int a=1,b=2,c=3, d;
+	
+	d=a+c;
+	
+
+	printf("El valor de la letra D: %d",d);
+	//return 0;
+}
